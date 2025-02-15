0c865512800e9bf494170d4a0c1bed912588c627
hima9414ch <chauhan1232000@gmail.com>
2025-02-15 23:20:23 +0530

    Update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,7 +3,7 @@
 // ```jsx
 import React from 'react';
 import Header from './Header';
-import HeroSection from './HeroSection';
+import {HeroSection} from './HeroSection';
 // import PropertyList from './PropertyList';
 // import SearchFilters from './SearchFilters';
 // import ContactForm from './ContactForm';
