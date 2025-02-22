5bde5bc7723aabec1e0ee874ada64d5338c53642
Himanshu Chauhan <Himanshu Chauhan>
2025-02-22 19:13:12 +0530

    Updated React components from JSON

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,31 +1,23 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 import HeroSection from './HeroSection';
-import FeaturedProperties from './FeaturedProperties';
-import SearchFilters from './SearchFilters';
 import PropertiesListing from './PropertiesListing';
 import PropertyDetails from './PropertyDetails';
-import ContactForm from './ContactForm';
 import Login from './Login';
 import Registration from './Registration';
-import UserSettings from './UserSettings';
-import ListingsControl from './ListingsControl';
-import Footer from './Footer';
+import Dashboard from './Dashboard';
 
 const Body = () => {
   return (
     <div>
       <Header />
       <HeroSection />
-      <FeaturedProperties />
-      <SearchFilters />
       <PropertiesListing />
       <PropertyDetails />
-      <ContactForm />
       <Login />
       <Registration />
-      <UserSettings />
-      <ListingsControl />
+      <Dashboard />
       <Footer />
     </div>
   );
