6eaa5f5b1dbdbd2dbfabf86edc2e30ac2e982204
Himanshu Chauhan <Himanshu Chauhan>
2025-02-26 22:59:30 +0530

    Updated React components from JSON

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-    import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-    import HomePage from "./HomePage/Body.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Global/Header.jsx";
+import Footer from "./Global/Footer.jsx";
+import HomePage from "./HomePage/Body.jsx";
 
-    const Body = () => {
-    return (
-        <Router>
+const Body = () => {
+return (
+    <Header />
+      <Router>
         <Routes>
             <Route path="/homepage" element={<HomePage />} />
         </Routes>
-        </Router>
-    );
-    };
+      </Router>
+    <Footer />
+);
+};
 
-    export default Body;
+export default Body;
