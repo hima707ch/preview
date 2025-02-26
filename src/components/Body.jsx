9e61a8cff33f63ea4b8440c244bf1f9fd3b8284f
Himanshu Chauhan <Himanshu Chauhan>
2025-02-26 22:50:29 +0530

    Updated React components from JSON

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import HeroSection from './HeroSection';
-import PropertiesListing from './PropertiesListing';
-import PropertyDetails from './PropertyDetails';
-import Login from './Login';
-import Registration from './Registration';
-import Dashboard from './Dashboard';
-import HomePage from './HomePage/Body.jsx'
-import PropertyListingPage from './PropertyListingPage/Body.jsx'
+    import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+    import HomePage from "./HomePage/Body.jsx";
 
-const Body = () => {
-  return (
-    <div>
-      <HomePage/>
-      <PropertyListingPage/>
-    </div>
-  );
-};
+    const Body = () => {
+    return (
+        <Router>
+        <Routes>
+            <Route path="/homepage" element={<HomePage />} />
+        </Routes>
+        </Router>
+    );
+    };
 
-export default Body;
+    export default Body;
