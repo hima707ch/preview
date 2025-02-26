5f1b056fcd782928df06c8fde81ccb2889116d80
hima9414ch <chauhan1232000@gmail.com>
2025-02-26 15:04:10 +0530

    Update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,18 +7,14 @@ import PropertyDetails from './PropertyDetails';
 import Login from './Login';
 import Registration from './Registration';
 import Dashboard from './Dashboard';
+import HomePage from './HomePage/Body.jsx'
+import PropertyListingPage from './PropertyListingPage/Body.jsx'
 
 const Body = () => {
   return (
     <div>
-      <Header />
-      <HeroSection />
-      <PropertiesListing />
-      <PropertyDetails />
-      <Login />
-      <Registration />
-      <Dashboard />
-      <Footer />
+      <HomePage/>
+      <PropertyListingPage/>
     </div>
   );
 };
