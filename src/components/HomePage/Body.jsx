25a061d11d0b7485bc63ef3f63e27c10740c6050
hima9414ch <chauhan1232000@gmail.com>
2025-02-26 15:00:40 +0530

    Update Body.jsx

--- src/components/HomePage/Body.jsx
+++ src/components/HomePage/Body.jsx
@@ -1,1 +1,26 @@
-import React from \'react\';\nimport HeroSection from \'./HeroSection\';\nimport NavigationMenu from \'./NavigationMenu\';\nimport ServicesSection from \'./ServicesSection\';\nimport Testimonials from \'./Testimonials\';\nimport Footer from \'./Footer\';\nimport NewsletterSignUp from \'./NewsletterSignUp\';\nimport FeaturedProducts from \'./FeaturedProducts\';\n\nconst Body = () => {\n  return (\n    <div className="body-container">\n      <NavigationMenu />\n      <HeroSection />\n      <ServicesSection />\n      <FeaturedProducts />\n      <Testimonials />\n      <NewsletterSignUp />\n      <Footer />\n    </div>\n  );\n};\n\nexport default Body;
+import React from "react";
+import HeroSection from "./HeroSection";
+import NavigationMenu from "./NavigationMenu";
+import ServicesSection from "./ServicesSection";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+import NewsletterSignUp from "./NewsletterSignUp";
+import FeaturedProducts from "./FeaturedProducts";
+
+const Body = () => {
+  return (
+    <main className="flex flex-col items-center w-full min-h-screen bg-gray-100">
+      <NavigationMenu />
+      <HeroSection />
+      <section className="container mx-auto px-4 py-8">
+        <ServicesSection />
+        <FeaturedProducts />
+      </section>
+      <Testimonials />
+      <NewsletterSignUp />
+      <Footer />
+    </main>
+  );
+};
+
+export default Body;
